7e4ada0dab97df636f56d1cb8e6351c563abf137
David Martinez <david.martinez@estudiantec.cr>
2021-04-11 17:55:54 -0600

    Added LCD_SCREEN

--- project_4/alarm.py
+++ project_4/alarm.py
@@ -1,6 +1,7 @@
 from registers import Registers
 from keyboard import Keyboard
 import threading
+from repeatedtimer import RepeatedTimer
 class Alarm(object):
     def __init__(self, view):
         self.view = view
@@ -8,6 +9,7 @@ class Alarm(object):
         self.state = "INITIAL_STATE"
         self.check_battery()
         self.view.lcd_error.setHidden(True)
+        self.rt = {}
         #Evaluate the ALARM_STATE.
         if(Registers.ALARM_STATE == "UNARMED"):
             self.start_state_unarmed()
@@ -20,6 +22,12 @@ class Alarm(object):
         print("ALARM in {} state".format(self.state))
     
     def key_pressed(self,value):
+        if(value == '3'):
+            self.blink_led(self.view.led_battery,1)
+        if(value == '4'):
+            self.stop_blink_led(self.view.led_battery)
+        if(value == '5'):
+            self.display_lcd('1234',3)
         if(self.state == "UNARMED" or self.state == "MODE_0" or self.state == "MODE_1" or self.state == "MAIN_ENTRANCE" or self.state == "ALARM"):
             self.keyboard.key_pressed(value)
         else:
@@ -96,6 +104,31 @@ class Alarm(object):
     
     def refresh_alarm(self):
         self.__init__(self.view)
+
+    def toggle_led(led_label):
+        led_label.setHidden(not led_label.isHidden())
+        
+
+    def blink_led(self,led_label, period):
+        print("Starting {}s timer for led {}".format(period,led_label.objectName()))
+        self.rt[led_label.objectName()] = RepeatedTimer(period, Alarm.toggle_led, led_label)
+
+
+    def stop_blink_led(self,led_label):
+        self.rt[led_label.objectName()].stop()
+
+    #if period == 0 message will be shown until cleared or overwriten.
+    def display_lcd(self,message,period):
+        self.view.lcd_screen.display(message)
+        if(period != 0):
+            self.lcd_timer = threading.Timer(period, self.clear_display)
+            print("Starting {}s timer for LCD".format(period))
+            self.lcd_timer.start()
+
+    def clear_display(self):
+        self.view.lcd_screen.display('')
+
+
             
 
         
